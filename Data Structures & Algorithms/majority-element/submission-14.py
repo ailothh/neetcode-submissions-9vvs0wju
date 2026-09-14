2e52614956e53482a090f9797c28e92e95b48844
ailothh <156462384+ailothh@users.noreply.github.com>
2026-09-14 10:46:41 -0400

    Add: majority-element - submission-14

---
+++ Data Structures & Algorithms/majority-element/submission-14.py
@@ -0,0 +1,6 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dic={}
+        for num in nums:
+            dic[num]=dic.get(num, 0)+1
+        return max(dic,key=dic.get)
